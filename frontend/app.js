1da5cb76b4b0c01df3e182385977035e2c669d64
nado-libya <jggbhfbt@gmail.com>
2026-04-19 03:13:27 +0200

    Upload initial setup files for the frontend using React.js

---
+++ frontend/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <h1>Welcome to React App!</h1>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
